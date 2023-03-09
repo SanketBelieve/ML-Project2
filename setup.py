9d705def12c013f106b841f318ab051c255f2b60
Sanket <alanesanket8@gmail.com>
2023-03-09 23:37:48 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->list[str]:
+    '''
+    this function will return list of requirements
+    '''
+    requirements=[]
+    with open (file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements        
+
+setup(
+name='ML-project2',
+version='0.0.1',
+author='Sanket',
+author_email='alanesanket8@gmail.com',
+find_packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
